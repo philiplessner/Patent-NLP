fa20dd897f4377c0dce210c1c4e250b747629310
philiplessner <philiplessner@gmail.com>
2017-01-02 10:27:34 -0500

    added routines to load gensim dictionary, corpus, index, xgrams, and serialize tokens

--- code/utilities.py
+++ code/utilities.py
@@ -1,4 +1,12 @@
 from itertools import islice
+import pickle
+from typing import List
+import gensim
+from gensim.models.word2vec import Word2Vec
+from gensim.models.phrases import Phrases
+from gensim.corpora.dictionary import Dictionary
+from gensim.corpora.mmcorpus import MmCorpus
+from gensim.similarities.docsim import MatrixSimilarity
 
 
 def save2file(filepath: str, tosave: str)->None:
@@ -20,3 +28,28 @@ def lines_from_file(filepath: str, line: int)->str:
 
 def save_model(filepath: str, model)->None:
     model.save(filepath)
+
+
+def load_xgram_model(filepath: str)->gensim.models.phrases.Phrases:
+    return Phrases.load(filepath)
+
+
+def load_w2v_model(filepath: str)->gensim.models.word2vec.Word2Vec:
+    return Word2Vec.load(filepath)
+
+
+def load_dictionary(filepath: str)->gensim.corpora.dictionary.Dictionary:
+    return Dictionary.load(filepath)
+
+
+def load_corpus(filepath: str)->gensim.corpora.mmcorpus.MmCorpus:
+    return MmCorpus(filepath)
+
+
+def load_index(filepath: str)->gensim.similarities.docsim.MatrixSimilarity:
+    return MatrixSimilarity.load(filepath)
+
+
+def serialize_tokens(filename: str, tokens: List[List[str]])->None:
+    with open(filename, 'wb') as f:
+        pickle.dump(tokens, f)
