a031f212226daedb965660d4c211e46bec1a90c9
philiplessner <philiplessner@gmail.com>
2017-01-08 06:59:49 -0500

    initial commit

---
+++ code/final_clean.py
@@ -0,0 +1,20 @@
+import re
+from toolz import compose
+import global_constants
+from utilities import read_file, save2file
+
+
+def remove_dualgrams(tobe_cleaned: str)->str:
+    return re.sub(r'(?P<first>[a-z]+)_(?P=first)', r'\1 \1', tobe_cleaned)
+
+
+def remove_singlechar(tobe_cleaned: str)->str:
+    return re.sub(r' [a-z] ', ' ', tobe_cleaned)
+
+
+if __name__ == '__main__':
+    cleaned = compose(remove_singlechar,
+                      remove_dualgrams,
+                      remove_dualgrams,
+                      read_file)(global_constants.TRI_SENTS)
+    save2file('../intermediate/trigram_sentences_cleaned.txt', cleaned)
