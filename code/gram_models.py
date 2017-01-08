622bccf739a856a869aff00489f175c2ec33a94f
philiplessner <philiplessner@gmail.com>
2017-01-08 07:01:27 -0500

    simplified functions

--- code/gram_models.py
+++ code/gram_models.py
@@ -1,7 +1,7 @@
-from typing import Iterator
+from typing import Iterator, List
 import spacy
 import gensim
-from gensim.models.phrases import Phrases
+from gensim.models.phrases import Phrases, Phraser
 from gensim.models.word2vec import LineSentence
 import global_constants
 from utilities import save2file, save_model
@@ -12,20 +12,20 @@ nlp = spacy.load('en')
 
 def doc2sents(filename: str)->str:
     with open(filename, 'r', encoding='utf-8') as f:
-        sentences = ''
+        sentences = []  # type: List[str]
         for doc in nlp.pipe((line.strip() for line in f),
-                            batch_size=2000, n_threads=3):
+                            batch_size=5000, n_threads=-1):
             for sent in doc.sents:
-                toks = (token.lemma_ for token in sent if not token.is_punct)
-                wostops = remove_stopwords('combined-stop-words.txt', toks)
-                newsent = ' '.join(list(wostops))
+                newsent = ' '.join((token.lemma_
+                                    for token in sent
+                                    if not token.is_punct))
                 newsent += '\n'
-                sentences += newsent
-    return sentences
+                sentences.append(newsent)
+    return ''.join(sentences)
 
 
 def xgram_model(filename: str)->gensim.models.phrases.Phrases:
-    return Phrases(LineSentence(filename))
+    return Phraser(Phrases(LineSentence(filename)))
 
 
 def remove_stopwords(stopfile: str, tokens: Iterator[str])->Iterator[str]:
@@ -34,16 +34,10 @@ def remove_stopwords(stopfile: str, tokens: Iterator[str])->Iterator[str]:
     return (token for token in tokens if token not in stops)
 
 
-def docs2xgram_docs(filename: str, xgram_model)->str:
-    with open(filename, 'r', encoding='utf-8') as f:
-        xgram_docs = ''
-        for doc in nlp.pipe((line for line in f),
-                            batch_size=2000, n_threads=3):
-            doctokens = (token.lemma_ for token in doc)
-            wostops = remove_stopwords('combined-stop-words.txt', doctokens)
-            xgram_doc = ' '.join(xgram_model[list(wostops)])
-            xgram_docs += xgram_doc
-    return xgram_docs
+def xgram_strings(filename: str,
+                  xgram_model: gensim.models.phrases.Phraser)->str:
+    return '\n'.join(' '.join(xgram_model[sentences])
+                     for sentences in LineSentence(filename))
 
 
 if __name__ == '__main__':
@@ -51,7 +45,10 @@ if __name__ == '__main__':
     save2file(global_constants.UNI_SENTS, sentences)
     bigram_model = xgram_model(global_constants.UNI_SENTS)
     save_model(global_constants.BI_MODEL, bigram_model)
-    bigram_docs = docs2xgram_docs(global_constants.SOURCE_FILE, bigram_model)
-    save2file(global_constants.BI_DOCS, bigram_docs)
-    bigram_sentences = doc2sents(global_constants.BI_DOCS)
+    bigram_sentences = xgram_strings(global_constants.UNI_SENTS,
+                                     bigram_model)
     save2file(global_constants.BI_SENTS, bigram_sentences)
+    trigram_model = xgram_model(global_constants.BI_SENTS)
+    save_model(global_constants.TRI_MODEL, trigram_model)
+    trigram_sentences = xgram_strings(global_constants.BI_SENTS, trigram_model)
+    save2file(global_constants.TRI_SENTS, trigram_sentences)
