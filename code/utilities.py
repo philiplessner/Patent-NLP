f5261d2417ba5f1ba739c84c521fa79dfb5693eb
philiplessner <philiplessner@gmail.com>
2017-01-08 19:52:33 -0500

    curried some functions. Returned file pathas string from save2file so it can be composed.

--- code/utilities.py
+++ code/utilities.py
@@ -1,6 +1,7 @@
 from itertools import islice
 import pickle
 from typing import List
+from toolz import curry
 import gensim
 from gensim.models.word2vec import Word2Vec
 from gensim.models.phrases import Phrases
@@ -9,9 +10,17 @@ from gensim.corpora.mmcorpus import MmCorpus
 from gensim.similarities.docsim import MatrixSimilarity
 
 
-def save2file(filepath: str, tosave: str)->None:
+@curry
+def save2file(filepath: str, tosave: str)->str:
+    '''Save a string to a file.
+    Parameters
+        filepath: full path to file relative to calling program
+        tosave: string to save to file
+        filepath: passed through for use in subsequent procedures
+    '''
     with open(filepath, 'w', encoding='utf-8') as f:
         f.write(tosave)
+    return filepath
 
 
 def read_file(filepath: str)->str:
@@ -26,7 +35,8 @@ def lines_from_file(filepath: str, line: int)->str:
     return contents[0]
 
 
-def save_model(filepath: str, model)->None:
+@curry
+def save_model(filepath: str, model):
     model.save(filepath)
 
 
