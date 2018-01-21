0c9c1e4c02ff5ee660ed31de33ca12e90294a8af
philiplessner <philiplessner@gmail.com>
2018-01-21 07:06:58 -0500

    refactoring

--- ipynb/common.py
+++ ipynb/common.py
@@ -1,7 +1,8 @@
 import os
 import tarfile
 import fnmatch
-from typing import Iterator
+from typing import Iterator, Pattern
+from cytoolz import curry
 
 
 def file_find(filepat: str, top: str) -> Iterator[str]:
@@ -31,3 +32,15 @@ def get_archivexml(filepath: str) -> Iterator[str]:
         yield f.read().decode(encoding='utf-8')
         f.close()
     tar.close()
+
+
+@curry
+def remove(pattern: Pattern[str], docstr: str) -> str:
+    '''Replace a regular expression pattern with a space
+    Parameters
+        pattern: a compiled re pattern
+        docstr: string to perform replacement on
+    Returns
+        string with pattern replaced by whitespace
+    '''
+    return pattern.sub(' ', docstr)
