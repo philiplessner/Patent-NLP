525b83b8184efb9789a06208fd136c5bc81a54af
philiplessner <philiplessner@gmail.com>
2018-03-18 15:28:24 +0000

    changed to add numbers as prefix to file with leading zero for <10. Added options for bucket and output file path.

--- code/split_file.py
+++ code/split_file.py
@@ -1,6 +1,6 @@
-import sys
+import argparse
 import os
-from typing import Iterator
+from typing import Iterator, Optional
 import smart_open
 
 
@@ -16,11 +16,19 @@ def get_doc(infile: str) -> Iterator[str]:
             yield line
 
 
-def file2multi(infile: str) -> None:
+def file2multi(infile: str, bucket: Optional[str],
+               outfile_path: Optional[str]) -> None:
     count = 0
-    path, filename = os.path.split(infile)
+    if bucket:
+        infile = '/'.join(['s3:/', bucket, infile])
+    if outfile_path:
+        path = '/'.join(['s3:/', bucket, outfile_path])
+        _, filename = os.path.split(infile)
+    else:
+        path, filename = os.path.split(infile)
+    print('Reading From:', infile)
     root = os.path.splitext(filename)[0]
-    outfile = os.path.join(path, root + str(count) + '.txt')
+    outfile = os.path.join(path, '{:02d}'.format(count) + root + '.txt')
     f = smart_open.smart_open(outfile, 'w', encoding='utf-8')
     for index, doc in enumerate(get_doc(infile)):
         if (index % 10000):
@@ -28,7 +36,7 @@ def file2multi(infile: str) -> None:
         else:
             f.close()
             count += 1
-            outfile = os.path.join(path, root + str(count) + '.txt')
+            outfile = os.path.join(path, '{:02d}'.format(count) + root + '.txt')
             print('Writing to:', outfile, '\n')
             f = smart_open.smart_open(outfile, 'w', encoding='utf-8')
             f.write(doc)
@@ -36,5 +44,13 @@ def file2multi(infile: str) -> None:
 
 
 if __name__ == '__main__':
-    infile = sys.argv[1]
-    file2multi(infile)
+    description = '''Split a raw text file into
+                     sequentially numbered separate files.
+                  '''
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=description)
+    parser.add_argument('filename')
+    parser.add_argument('-b', '--bucket', help='S3 Bucket Name')
+    parser.add_argument('-o', '--output', help='Output Path')
+    args = parser.parse_args()
+    file2multi(args.filename, args.bucket, args.output)
