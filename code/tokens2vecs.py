556679c16f814f3c6e2ff6444b24af48232e801a
philiplessner <philiplessner@gmail.com>
2018-03-30 23:41:33 +0000

    changed get_tokens to iterate over a list of files. Created function make_dict to make dictionary.

--- code/tokens2vecs.py
+++ code/tokens2vecs.py
@@ -2,26 +2,28 @@ import os
 import argparse
 import fnmatch
 from typing import Iterator, List
+import gensim
 from gensim import corpora
 import time
 import boto
 import smart_open
 
 
-def get_tokens(infile: str) -> Iterator[str]:
-    '''Yield new line terminated strings with newlines stripped
+def get_tokens(infiles: List[str]) -> Iterator[str]:
+    '''Yield list of strings from each line in each file
     Parameters
-        infile: full path string to file containing strings
+        infiles: list of full path strings to file containing strings
     Returns
-        each string with newline stripped
+        list of strings with newline stripped
     '''
-    with smart_open.smart_open(infile, 'r', encoding='utf-8') as inf:
-        for line in inf:
-            yield line.strip().split()
+    for infile in infiles:
+        with smart_open.smart_open(infile, 'r', encoding='utf-8') as inf:
+            for line in inf:
+                yield line.strip().split()
 
 
-def list_file(mybucket: str, prefix: str) -> List[str]:
-    '''List files in an Amazon S3 Bucket
+def list_files(mybucket: str, prefix: str) -> List[str]:
+    '''List files in a directory in an Amazon S3 Bucket
        Parameters
             mybucket: name of bucket
             prefix: directory in bucket
@@ -32,39 +34,50 @@ def list_file(mybucket: str, prefix: str) -> List[str]:
     return [key.name for key in bucket.list(prefix=prefix)]
 
 
-if __name__ == '__main__':
-    description = '''Vectorize tokens
-                  '''
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=description)
-    parser.add_argument('filename')
-    parser.add_argument('-b', '--bucket', help='S3 Bucket Name')
-    parser.add_argument('-d', '--dictionary',
-                        help='Full Path to Dictionary File')
-    parser.add_argument('-v', '--vector',
-                        help='Full Path to Vector File')
-    args = parser.parse_args()
-    filepaths = list_file('pto-us-data', 'token-text')
+def match_files(matchfiles: str,
+                allfiles: List[str], bucket: str) -> List[str]:
     infiles = []
-    for fname in fnmatch.filter(filepaths, args.filename):
-        if args.bucket:
-            infile = '/'.join(['s3:/', args.bucket, fname])
+    for fname in fnmatch.filter(allfiles, matchfiles):
+        if bucket:
+            infile = '/'.join(['s3:/', bucket, fname])
             print(infile)
         else:
             infile = '/'.join(['s3:/', fname])
             print(infile)
             path, filename = os.path.split(infile)
         infiles.append(infile)
+    return infiles
+
+
+def make_dict(infiles: List[str]) -> gensim.corpora.Dictionary:
     start = time.time()
-    term_dict = corpora.Dictionary([tokens for tokens in get_tokens(infile)
-                                    for infile in infiles])
+    term_dict = corpora.Dictionary([tokens for tokens in get_tokens(infiles)])
     if args.dictionary:
         with smart_open.smart_open(args.dictionary, 'wb') as fout:
             term_dict.save(fout)
     end = time.time()
     delta = end - start
     print('Time to Make Dictionary', delta, ' sec')
+    return term_dict
+
+
+if __name__ == '__main__':
+    description = '''Vectorize tokens
+                  '''
+    formatter_class = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=formatter_class,
+                                     description=description)
+    parser.add_argument('filename', help='glob of filenames')
+    parser.add_argument('-b', '--bucket', help='S3 Bucket Name')
+    parser.add_argument('-d', '--dictionary',
+                        help='Full Path to Dictionary File')
+    parser.add_argument('-v', '--vector',
+                        help='Full Path to Vector File')
+    args = parser.parse_args()
+    allfiles = list_files('pto-us-data', 'token-text')
+    infiles = match_files(args.filename, allfiles, args.bucket)
+    term_dict = make_dict(infiles)
     corpus = [term_dict.doc2bow(tokens)
-              for tokens in get_tokens(infile) for infile in infiles]
+              for tokens in get_tokens(infiles)]
     if args.vector:
         corpora.MmCorpus.serialize(args.vector, corpus, id2word=term_dict)
