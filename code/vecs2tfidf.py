6e7133f68e6b17d63355d1b8a055d3429ae3e670
philiplessner <philiplessner@gmail.com>
2018-03-31 20:29:47 +0000

    more logging

--- code/vecs2tfidf.py
+++ code/vecs2tfidf.py
@@ -36,11 +36,18 @@ if __name__ == '__main__':
     logger = setup_logger()
     logger.info('%s%s', 'Running ', sys.argv[0])
     corpus = corpora.MmCorpus(args.filename)
+    logger.info('%s%s', 'Reading MMCorpus File From ', args.filename)
     if args.dictionary:
+        logger.info('%s%s',
+                    'Reading Corpus Dictionary File From ',
+                    args.dictionary)
         corpora_dict = corpora.Dictionary.load(args.dictionary)
     tfidf_model = models.TfidfModel(corpus,
                                     id2word=corpora_dict,
                                     dictionary=corpora_dict,
                                     normalize=True)
+    logger.info('%s', 'Finished TfIdf Model')
     tfidf_model.save(args.tfidf)
+    logger.info('%s%s', 'Saved TfIdf Model To ', args.tfidf)
     corpus_tfidf = tfidf_model[corpus]
+    logger.info('%s', 'Finished Transforming Corpus to TfIdf Vectors')
